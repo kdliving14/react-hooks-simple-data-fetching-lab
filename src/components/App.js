e9ae3278ce29dce54c49c1dbf629082dbcde361e
Karter <kdlmuzixnerd14@gmail.com>
2022-07-25 14:25:41 -0700

    commit tree 49af89c0b0f6f6425d4572d49adc6fa014256dac

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React from 'react'
+import {useEffect} from "react";
+import {useState} from "react";
+
+const App = () => {
+  
+  const [dogURL, setDogURL] = useState("")
+
+  useEffect(() => 
+	{
+	    fetch("https://dog.ceo/api/breeds/image/random")
+	      .then((data) => data.json())
+	      .then((data) => {setDogURL(data.message);});
+  }, []);
+
+  if(!dogURL) return <p>Loading...</p>
+  
+  return <img src={dogURL} alt="A Random Dog"/>
+}
+
+export default App
